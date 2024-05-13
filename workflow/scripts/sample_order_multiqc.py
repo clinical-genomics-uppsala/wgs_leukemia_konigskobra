b48f02fdebaf7f5418d446e467d17cd36732207d
Arielle R Munters <arielle@reivant.se>
2024-05-13 14:14:21 +0200

    fix: spelling

--- workflow/scripts/sample_order_multiqc.py
+++ workflow/scripts/sample_order_multiqc.py
@@ -21,14 +21,14 @@ sample_order = [list(x) for x in set(tuple(x) for x in sample_order_duplicates)]
 sample_order.sort(key=lambda x: int(x[1]))
 
 with open(snakemake.output.replacement_dna, "w+") as replacement_dna, \
-     open(snakemake.output.replacement_rna, "w+") as replacement_dna, \
-     open(snakemake.output.order_dna, "w+") as order_tsv_dna, \
-     open(snakemake.output.order_rna, "w+") as order_tsv_rna, \
+     open(snakemake.output.replacement_rna, "w+") as replacement_rna, \
+     open(snakemake.output.order_dna, "w+") as order_dna, \
+     open(snakemake.output.order_rna, "w+") as order_rna, \
      open(snakemake.output.dnanumber, "w+") as dna_table, \
      open(snakemake.output.rnanumber, "w+") as rna_table:
 
-    order_tsv_dna.write("\t".join(["Sample Order", "Pedegree ID", "DNA number"])+"\n")
-    order_tsv_rna.write("\t".join(["Sample Order", "Pedegree ID", "RNA number"])+"\n")
+    order_dna.write("\t".join(["Sample Order", "Pedegree ID", "DNA number"])+"\n")
+    order_rna.write("\t".join(["Sample Order", "Pedegree ID", "RNA number"])+"\n")
     dna_table.write("\t".join(["Sample", "ped_id", "dna_number"])+"\n")
     rna_table.write("\t".join(["Sample", "ped_id", "rna_number"])+"\n")
 
@@ -39,12 +39,12 @@ with open(snakemake.output.replacement_dna, "w+") as replacement_dna, \
         order_line = [sample_line[sample_order_index.index("sample")], sample_line[sample_order_index.index("lab_id")]]
         if (sample_line[sample_order_index.index("type")].lower() == "t" or
                 sample_line[sample_order_index.index("type")].lower() == "n"):
-            replacement_tsv_dna.write("\t".join(replacement_line + ["sample_"+str(f"{i:03}")]) + "\n")
-            order_tsv_dna.write("\t".join(["sample_"+str(f"{i:03}")] + order_line) + "\n")
+            replacement_dna.write("\t".join(replacement_line + ["sample_"+str(f"{i:03}")]) + "\n")
+            order_dna.write("\t".join(["sample_"+str(f"{i:03}")] + order_line) + "\n")
             dna_table.write("\t".join(["sample_"+str(f"{i:03}")] + order_line)+"\n")
             i += 1
         elif sample_line[sample_order_index.index("type")].lower() == "r":
-            replacement_tsv_rna.write("\t".join(replacement_line + ["sample_"+str(f"{j:03}")]) + "\n")
-            order_tsv_rna.write("\t".join(["sample_"+str(f"{j:03}")] + order_line) + "\n")
+            replacement_rna.write("\t".join(replacement_line + ["sample_"+str(f"{j:03}")]) + "\n")
+            order_rna.write("\t".join(["sample_"+str(f"{j:03}")] + order_line) + "\n")
             rna_table.write("\t".join(["sample_"+str(f"{j:03}")] + order_line) + "\n")
             j += 1
