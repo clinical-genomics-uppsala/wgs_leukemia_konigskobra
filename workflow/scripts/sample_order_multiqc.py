7738d4faf9e193febc0180de9ca30feda2ab8efa
Arielle R Munters <arielle@reivant.se>
2024-05-13 15:28:44 +0200

    style: small style changes

--- workflow/scripts/sample_order_multiqc.py
+++ workflow/scripts/sample_order_multiqc.py
@@ -16,10 +16,10 @@ for sample, type, fastq_path in snakemake.params.filelist:
     try:
         s_index = int(s_pattern.search(fastq).group(1))
     except:
-        s_index=99
+        s_index = 99
     # If same sample sequenced twice use latest runs s_index
     for old_sample, old_s, old_lab, old_type in sample_order_duplicates:
-        if (old_sample == sample + "_" + type and old_lab == lab_id and s_index == 99):
+        if old_sample == sample + "_" + type and old_lab == lab_id and s_index == 99:
             s_index = old_s
     sample_order_duplicates.append([sample + "_" + type, s_index, lab_id, type])
 
@@ -34,10 +34,10 @@ with open(snakemake.output.replacement_dna, "w+") as replacement_dna, \
      open(snakemake.output.dnanumber, "w+") as dna_table, \
      open(snakemake.output.rnanumber, "w+") as rna_table:
 
-    order_dna.write("\t".join(["Sample Order", "Pedegree ID", "DNA number"])+"\n")
-    order_rna.write("\t".join(["Sample Order", "Pedegree ID", "RNA number"])+"\n")
-    dna_table.write("\t".join(["Sample", "ped_id", "dna_number"])+"\n")
-    rna_table.write("\t".join(["Sample", "ped_id", "rna_number"])+"\n")
+    order_dna.write("\t".join(["Sample Order", "Pedegree ID", "DNA number"]) + "\n")
+    order_rna.write("\t".join(["Sample Order", "Pedegree ID", "RNA number"]) + "\n")
+    dna_table.write("\t".join(["Sample", "ped_id", "dna_number"]) + "\n")
+    rna_table.write("\t".join(["Sample", "ped_id", "rna_number"]) + "\n")
 
     i = 1
     j = 1
@@ -45,15 +45,15 @@ with open(snakemake.output.replacement_dna, "w+") as replacement_dna, \
         replacement_line = [sample_line[sample_order_index.index("sample")]]
         order_line = [sample_line[sample_order_index.index("sample")], sample_line[sample_order_index.index("lab_id")]]
         if (
-            sample_line[sample_order_index.index("type")].lower() == "t" or
-            sample_line[sample_order_index.index("type")].lower() == "n"
+            sample_line[sample_order_index.index("type")].lower() == "t"
+            or sample_line[sample_order_index.index("type")].lower() == "n"
         ):
-            replacement_dna.write("\t".join(replacement_line + ["sample_"+str(f"{i:03}")]) + "\n")
+            replacement_dna.write("\t".join(replacement_line + ["sample_" + str(f"{i:03}")]) + "\n")
             order_dna.write("\t".join(["sample_" + str(f"{i:03}")] + order_line) + "\n")
             dna_table.write("\t".join(["sample_" + str(f"{i:03}")] + order_line) + "\n")
             i += 1
         elif sample_line[sample_order_index.index("type")].lower() == "r":
-            replacement_rna.write("\t".join(replacement_line + ["sample_"+str(f"{j:03}")]) + "\n")
+            replacement_rna.write("\t".join(replacement_line + ["sample_" + str(f"{j:03}")]) + "\n")
             order_rna.write("\t".join(["sample_" + str(f"{j:03}")] + order_line) + "\n")
             rna_table.write("\t".join(["sample_" + str(f"{j:03}")] + order_line) + "\n")
             j += 1
