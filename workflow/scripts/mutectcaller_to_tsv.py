5918aae31358bc997d38904ee014a09ba1849ff2
Arielle R Munters <arielle@reivant.se>
2024-08-20 10:54:01 +0200

    refactor: update mutect to tsv script to include normal af and dp for tn samples

--- workflow/scripts/mutectcaller_to_tsv.py
+++ workflow/scripts/mutectcaller_to_tsv.py
@@ -3,30 +3,134 @@
 
 import csv
 import sys
-import vcf
+from pysam import VariantFile
+import logging
 
-input_file = snakemake.input.vcf
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.INFO,
+)
+
+input_file = VariantFile(snakemake.input.vcf)
 analysis = snakemake.params.analysis
-sample = snakemake.params.sample
 output_file = snakemake.output.tsv
 
-vcf = vcf.Reader(open(input_file, "r"))
 
+csq_index = []
+for x in input_file.header.records:
+    if "CSQ" in str(x):
+        csq_index = str(x).split("Format: ")[1].strip().strip('">').split("|")
+
+outlines = []
+if len(list(input_file.header.samples)) == 1:
+    logging.debug("One sample identified. " + f"{list(input_file.header.samples)=}")
+    outlines.append(
+        [
+            "#SAMPLE",
+            "CHROMOSOME",
+            "POSITION",
+            "REFERENCE",
+            "ALTERNATIVE",
+            "ALLELEFREQUENCY",
+            "DEPTH",
+            "GENE",
+            "TRANSCRIPT",
+            "NUCLEOTIDESEQ",
+            "PROTEIN",
+            "PROTEINSEQ",
+            "CONSEQUENCE",
+        ]
+    )
+
+elif len(list(input_file.header.samples)) == 2:
+    logging.debug("Two sample identified. " + f"{list(input_file.header.samples)=}")
+    outlines.append(
+        [
+            "#SAMPLE",
+            "CHROMOSOME",
+            "POSITION",
+            "REFERENCE",
+            "ALTERNATIVE",
+            "ALLELEFREQUENCY",
+            "NORMAL_AF",
+            "DEPTH",
+            "NORMAL_DP",
+            "GENE",
+            "TRANSCRIPT",
+            "NUCLEOTIDESEQ",
+            "PROTEIN",
+            "PROTEINSEQ",
+            "CONSEQUENCE",
+        ]
+    )
+else:
+    logging.error(snakemake.input.vcf + " contains more than two samples in header.")
+    sys.exit()
+
+
+logging.info("Starting to iterate through calls in " + snakemake.input.vcf)
+for row in input_file.fetch():
+    csq = row.info["CSQ"][0].split("|")
+    gene = csq[csq_index.index("SYMBOL")]
+
+    transcript_name = csq[csq_index.index("HGVSc")].split(":")[0]
+    transcript_change = ""
+    if len(csq[csq_index.index("HGVSc")].split(":")) > 1:
+        transcript_change = csq[csq_index.index("HGVSc")].split(":")[1]
+
+    protein_name = csq[csq_index.index("HGVSp")].split(":")[0]
+    protein_change = ""
+    if len(csq[csq_index.index("HGVSp")].split(":")) > 1:
+        protein_change = csq[csq_index.index("HGVSp")].split(":")[1]
+
+    consequence = csq[csq_index.index("Consequence")]
+
+    tumor_sample = [x for x in row.samples if x.endswith("_T")][0]
+    af = row.samples[tumor_sample]["AF"][0]
+    dp = row.samples[tumor_sample]["DP"][0]
+    outline = [
+        tumor_sample.split("_")[0],
+        row.chrom,
+        row.pos,
+        row.ref,
+        row.alts[0],
+        af,
+        dp,
+        gene,
+        transcript_name,
+        transcript_change,
+        protein_name,
+        protein_change,
+        consequence,
+    ]
+
+    if len(list(input_file.header.samples)) == 2:
+        normal_sample = [x for x in row.samples if x.endswith("_N")][0]
+        af_normal = row.samples[normal_sample]["AF"][0]
+        dp_normal = row.samples[normal_sample]["DP"][0]
+        outline = [
+            tumor_sample.split("_")[0],
+            row.chrom,
+            row.pos,
+            row.ref,
+            row.alts[0],
+            af,
+            af_normal,
+            dp,
+            dp_normal,
+            gene,
+            transcript_name,
+            transcript_change,
+            protein_name,
+            protein_change,
+            consequence,
+        ]
+
+    outlines.append(outline)
+
+logging.info("Writing results to " + snakmake.output.tsv)
 with open(output_file, "wt") as tsv:
-    tsv_writer = csv.writer(tsv, delimiter='\t')
-    tsv_writer.writerow(["#SAMPLE", "CHROMOSOME", "POSITION", "REFERENCE", "ALTERNATIVE", "ALLELEFREQUENCY", "DEPTH", "GENE",
-                         "TRANSCRIPT", "NUCLEOTIDESEQ", "PROTEIN", "PROTEINSEQ", "CONSEQUENCE"])
-    for row in vcf:
-        genes = row.INFO["CSQ"][0].split("|")
-        transcript_field = genes[10].split(":")
-        if len(transcript_field) == 1:
-            transcript_field = ["", ""]
-        protein_field = genes[11].split(":")
-        if len(protein_field) == 1:
-            protein_field = [genes[24], ""]
-        i = 0
-        if analysis == "tn":
-            i = 1
-        af = row.samples[i]["AF"]
-        tsv_writer.writerow([sample, row.CHROM, row.POS, row.REF, row.ALT, af, row.INFO["DP"], genes[3], transcript_field[0],
-                             transcript_field[1], protein_field[0], protein_field[1], genes[1]])
+    tsv_writer = csv.writer(tsv, delimiter="\t")
+    tsv_writer.writerows(outlines)
